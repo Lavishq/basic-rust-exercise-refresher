9aef8075382a84637f50b41f2e271632b5b49022
lavishq <96654752+Lavishq@users.noreply.github.com>
2024-05-25 14:48:46 +0200

    all exercise doen

--- bin/a18b.rs
+++ bin/a18b.rs
@@ -96,7 +96,16 @@ fn authorize(
     employee_name: &str,
     location: ProtectedLocation,
 ) -> Result<AuthorizationStatus, String> {
-    // put your code here
+    // put your code here // i wrote some code by myself and watched video then tried to solve myself and did it quickly w/ 1 compiler error
+    let db = Database::connect()?;
+    let employee = db.find_employee(employee_name)?;
+    let keycard = db.get_keycard(&employee)?;
+
+    if keycard.access_level >= location.required_access_level() {
+        Ok(AuthorizationStatus::Allow)
+    } else {
+        Ok(AuthorizationStatus::Deny)
+    }
 }
 
 fn main() {
--- bin/a19.rs
+++ bin/a19.rs
@@ -13,4 +13,26 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
+
+fn main() {
+    let mut storage = HashMap::new();
+    storage.insert("Chair", 5);
+    storage.insert("Bed", 3);
+    storage.insert("Table", 2);
+    storage.insert("Couch", 0);
+
+    let mut sum_of_items = 0;
+
+    for (item, amount) in storage {
+        sum_of_items += 0;
+        // i used if else but then peeked soln and if let is a better choice... cheated format and println
+        let stock_count = if amount == 0 {
+            "out of stock".to_owned()
+        } else {
+            format!("{:?}", amount)
+        };
+        println!("item={:?}, stock={:?}", item, stock_count);
+    }
+    println!("total stock={:?}", sum_of_items);   
+}
